8627c7c4ca25d6e6973012ba3bfdaf5405f87ace
JaiRaga <raga.j.santhosh@gmail.com>
2022-02-20 23:04:05 +0530

    :sparkles: created register form

--- components/auth/Register.js
+++ components/auth/Register.js
@@ -1,10 +1,22 @@
-import { StyleSheet, Text, View, StatusBar } from 'react-native'
-import React from 'react'
+import React, {useState} from 'react'
+import { StyleSheet, Text, View, StatusBar, TextInput, Button } from 'react-native'
+
 
 const Register = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [name, setName] = useState('')
+
+  const onSignUp = () => {
+    console.log("Done")
+  }
+
 	return (
 		<View style={styles.container}>
-			<Text>Register</Text>
+      <TextInput placeholder="" onChangeText={ name => setName(name)} />
+      <TextInput placeholder="" onChangeText={email => setEmail(email)} />
+      <TextInput placeholder="" onChangeText={password => setPassword(password)} secureTextEntry={true} />
+      <Button title="Sign up" onPress={onSignUp} />
 		</View>
 	)
 }
@@ -13,6 +25,7 @@ export default Register
 
 const styles = StyleSheet.create({
 	container: {
-		marginTop: StatusBar.currentHeight,
+    marginTop: StatusBar.currentHeight,
+    padding: 5
 	},
 })
